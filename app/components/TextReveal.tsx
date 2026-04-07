c0efb7728b5da51126000ccaef72699b596ed37f
sheryyiee121 <mylove0012211@gmail.com>
2026-04-07 15:55:04 +0500

    updated

--- app/components/TextReveal.tsx
+++ app/components/TextReveal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, type JSX } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
